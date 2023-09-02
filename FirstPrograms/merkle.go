288acfc432d07919e876c1d7f14fd83bc2d1f44e
mavcsquared <marco15432@gmail.com>
2023-09-02 18:13:14 -0300

    Non-working correct (I think) logic

--- FirstPrograms/merkle.go
+++ FirstPrograms/merkle.go
@@ -3,31 +3,103 @@ package main
 import (
 	"fmt"
 
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/hash/mimc"
 )
 
 type merkleCircuit struct {
-	rootHash          frontend.Variable `gnark:",public"`
-	path, aux, leaves []frontend.Variable
-	leaf              frontend.Variable `gnark:",public"`
+	rootHash    frontend.Variable `gnark:",public"`
+	path        []int
+	leaf        string `gnark:",public"`
+	index, size int    `gnark:",public"`
+	leaves      []byte
 }
 
-func (circuit *merkleCircuit) isLeaf(api frontend.API, leaf frontend.Variable, index int) {
+func (circuit *merkleCircuit) Define(api frontend.API) error {
 
-}
+	circuit.path = getPath(circuit.index, circuit.size)
 
-func (circuit *merkleCircuit) Define(api frontend.API, index int) error {
-	mimc, _ := mimc.NewMiMC(api)
-	mimc.Write(circuit.rootHash)
-	//cosas que hacer
+	mimc := mimc.NewMiMC()
+	mimc.Write([]byte(circuit.leaf))
+	actual := mimc.Sum(nil)
 
-	circuit.isLeaf(api, circuit.leaf, index)
+	newRootHash := circuit.generateProof(circuit.index, actual)
+
+	api.AssertIsEqual(circuit.rootHash, newRootHash)
 
 	return nil
 }
 
+func (circuit *merkleCircuit) generateProof(index int, actual []byte) []byte {
+	mimc := mimc.NewMiMC()
+
+	if index == len(circuit.path) {
+		return actual
+	}
+
+	var otherByte []byte
+
+	if circuit.path[index] > 0 {
+		otherByte = getHash(index, circuit.size, circuit.leaves)
+		mimc.Write(append(actual, otherByte...))
+	} else {
+		otherByte = getHash(-index, circuit.size, circuit.leaves)
+		mimc.Write(append(otherByte, actual...))
+	}
+
+	actual = mimc.Sum(nil)
+
+	return circuit.generateProof(index+1, actual)
+}
+
+func getPath(index int, size int) []int {
+	var path []int
+
+	index = index + size
+
+	for {
+		if index == 1 {
+			break
+		}
+		if index%2 == 0 {
+			path = append(path, index+1)
+
+		} else {
+			path = append(path, -index+1)
+		}
+
+		index /= 2
+	}
+
+	return path
+}
+
+func getHash(index int, size int, leaves []byte) []byte {
+	mimc := mimc.NewMiMC()
+
+	if index >= size {
+		fmt.Println([]byte{leaves[index-size]})
+		mimc.Write([]byte{leaves[index-size]})
+		println("LLEGA")
+	} else {
+		byteLeft := getHash(index*2, size, leaves)
+		byteRight := getHash(index*2+1, size, leaves)
+
+		thisByte := append(byteLeft, byteRight...)
+		mimc.Write(thisByte)
+	}
+
+	return mimc.Sum(nil)
+}
+
+// /*
 func main() {
 	fmt.Println("BEGIN")
 
+	str := "abcdefgh"
+	byteFromStr := []byte(str)
+
+	mimc.NewMiMC().Write(byteFromStr[1:2])
 }
+
+// */
--- FirstPrograms/merkle_test.go
+++ FirstPrograms/merkle_test.go
@@ -1,28 +1,30 @@
 package main
 
-/*
 import (
 	"testing"
 
 	"github.com/consensys/gnark/test"
 )
 
-
-
 func TestMerkle(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	var merkle merkleCircuit
 
-	assert.ProverSucceded(&merkle, &merkleCircuit{
-		X: 42,
-		Y: 42,
+	str := "abcdefgh"
+
+	assert.ProverFailed(&merkle, &merkleCircuit{
+		leaf:     "c",
+		index:    1,
+		size:     len(str),
+		rootHash: getHash(1, len(str), []byte(str)),
 	})
 
 	assert.ProverSucceeded(&merkle, &merkleCircuit{
-		X: 3,
-		Y: 35,
+		leaf:     "c",
+		index:    3,
+		size:     len(str),
+		rootHash: getHash(3, len(str), []byte(str)),
 	})
 
 }
-*/
